394e93e2cc4c3064b685a4f29bc363e39c924043
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2016-05-09 12:41:11 +0500

    Devise: Congiured devise for haunts sign_in and sign_up

---
+++ app/models/haunt.rb
@@ -0,0 +1,6 @@
+class Haunt < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'hangers/show'
+  devise_for :haunts
+  
+  root to:'hangers#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
